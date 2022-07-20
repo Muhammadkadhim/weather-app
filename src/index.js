b57719cc898fd5b6c697c3b24205ac8101658240
Muhammadkadhim <muhammad.kadhim.hassan@gmail.com>
2022-07-21 01:20:40 +0300

    updated index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-// bootstrap-select
-import "bootstrap-select/dist/css/bootstrap-select.min.css";
-import "bootstrap-select/dist/js/bootstrap-select.min.js";
-
 // components
 import App from "./App";
 
